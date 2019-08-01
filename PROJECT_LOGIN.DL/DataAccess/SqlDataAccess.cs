9045efc9f11c93fe6473ceac69e5ca532722f245
miljandv <49723857+miljandv@users.noreply.github.com>
2019-08-01 17:31:26 +0300

    Add files via upload

---
+++ PROJECT_LOGIN.DL/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVC_test.DataAccess
+{
+    public class SqlDataAccess
+    {
+        static public string GetConnectionString(string connectionName = "PROJECT_LOGIN.DB")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+    }
+}
---
+++ PROJECT_LOGIN.DL/Models/RecordEntityModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_test.DL.Models
+{
+    public class RecordEntityModel
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Phone { get; set; }
+    }
+}
